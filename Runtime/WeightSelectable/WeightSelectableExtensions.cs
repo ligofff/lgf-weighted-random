fc399ce27c9a56e89a76427aa33eb98ba029f53d
ligofff <liga@ligofff.ru>
2023-12-14 08:10:20 +0300

    RandomByWeight - multiple elements selection overload added

--- Runtime/WeightSelectable/WeightSelectableExtensions.cs
+++ Runtime/WeightSelectable/WeightSelectableExtensions.cs
@@ -35,6 +35,49 @@ namespace WeightSelectable
 
             throw new InvalidOperationException("The execution should never reach here.");
         }
-    }
+        
+        public static List<T> RandomByWeight<T>(this IEnumerable<T> enumerable, int selectedCount) where T : IWeightSelectable
+        {
+            List<T> localCollection = enumerable.ToList();
+            
+            if (!localCollection.Any())
+            {
+                throw new ArgumentException("Empty collection");
+            }
 
+            if (localCollection.Count < selectedCount)
+            {
+                throw new ArgumentException("Number of elements to select should not exceed the total elements in the collection.");
+            }
+
+            float totalWeight = localCollection.Sum(item => item.Weight);
+
+            if (totalWeight <= 0)
+            {
+                throw new ArgumentException("Total weight must be greater than zero.");
+            }
+            
+            List<T> selectedItems = new List<T>();
+            
+            for (int i = 0; i < selectedCount; i++)
+            {
+                float choice = (float)random.NextDouble() * totalWeight;
+
+                for (int j = 0; j < localCollection.Count; j++)
+                {
+                    if (choice < localCollection[j].Weight)
+                    {
+                        selectedItems.Add(localCollection[j]);
+                        totalWeight -= localCollection[j].Weight;
+                        localCollection.RemoveAt(j);
+                        break;
+                    }
+
+                    choice -= localCollection[j].Weight;
+                }
+            }
+
+            return selectedItems;
+        }
+    }
 }
